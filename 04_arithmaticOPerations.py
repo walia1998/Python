f2c6e21f3f14d6c77ab38393e9abba87ee92a55f
Ashish Walia <waliaashu1998@gmail.com>
2025-05-11 16:59:32 +0530

    ifelse

--- 04_arithmaticOPerations.py
+++ 04_arithmaticOPerations.py
@@ -6,3 +6,5 @@ a-=6
 print(a)
 a/=6
 print(a)
+a**=6
+print(a)
---
+++ ControlsAndFlows/05_Ifelse.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age : "))
+
+if(age > 18 ):
+    print("you can drive")
+else:
+    print("you cannot drive")25
