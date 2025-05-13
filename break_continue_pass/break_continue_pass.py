ae6fb94241e2d10a535f29b2db5de4dfd32864b8
Ashish Walia <waliaashu1998@gmail.com>
2025-05-13 18:08:57 +0530

    pass statement

--- break_continue_pass/break_continue_pass.py
+++ break_continue_pass/break_continue_pass.py
@@ -2,3 +2,21 @@ for i in range (0 , 20):
     print(i)
     if(i == 15):
         break
+
+
+
+    # Continue
+
+    for i in range (0,25):
+        if i == 10:
+            continue #continue the loop for the next iteration here itself.
+        print(i)
+
+
+         pass
+
+i = 3
+
+if i == 12:
+    pass #do nothing 
+print("End of a program")
