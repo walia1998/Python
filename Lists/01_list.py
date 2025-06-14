9d93452f9f0d8f9654dcee0cc8af79159c35c94d
Ashish Walia <waliaashu1998@gmail.com>
2025-06-14 14:13:44 +0530

    List

---
+++ Lists/01_list.py
@@ -0,0 +1,8 @@
+marks = [56,69,69,26,26]
+extend_marks = [59,78,95]
+
+marks.extend(extend_marks)
+print(marks)
+
+marks.pop(2)
+print(marks)
---
+++ Lists/02_list_comprehension.py
@@ -0,0 +1,11 @@
+#create a list containing the table of 5
+# a = 5 
+# table = []
+
+# for i in range(1, 11):
+#     table.append(a*i)
+
+
+table = [5 * i for i in range(1,11)] #this is the example of list_Comprehension
+
+print(table)
