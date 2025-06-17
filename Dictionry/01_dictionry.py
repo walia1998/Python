134124386cb1d07e55c93c8c59dd8e5f46ddafa0
Ashish Walia <waliaashu1998@gmail.com>
2025-06-17 11:17:53 +0530

    dictonary

---
+++ Dictionry/01_dictionry.py
@@ -0,0 +1,9 @@
+marks = {"Ashish Walia" : 98, "Sania Walia" : 96, "PrikshitPiki" : 99}
+
+
+print(marks["Ashish Walia"])
+
+marks["Sania Walia"] = 100
+
+print(marks["Sania Walia"])
+print(marks)
---
+++ Dictionry/02_dictionry_methods.py
@@ -0,0 +1,5 @@
+marks = {"Ashish Walia" : 98, "Sania Walia" : 96, "PrikshitPiki" : 99}
+
+print(marks.keys())
+print(marks.values())
+print(marks.items())
