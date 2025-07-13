38439eace656fb755fc2f1422442762bbe6e75f0
Ashish Walia <waliaashu1998@gmail.com>
2025-07-13 14:55:46 +0530

    raise method

--- Advance_Python Concepts/06_error.py
+++ Advance_Python Concepts/06_error.py
@@ -1,7 +1,25 @@
-while True : 
-    try:
-        a = int(input("Enter number 1 :"))
-        b = int(input("Enter number 2 :"))
-        print(f"The sum is {a + b}")    
-    except Exception as e :
-        print("Some error has occured", e)
+# while True : 
+    # try:
+    #     a = int(input("Enter number 1 :"))
+    #     b = int(input("Enter number 2 :"))
+    #     print(f"The sum is {a / b}")    
+
+    # except ValueError:
+    #     print("Please dont perform bad typecast")
+
+    # except ZeroDivisionError:
+    #     print("please dont divide by 0")
+
+    # except Exception as e :
+    #     print("Some error has occured", e)
+
+
+
+
+a = int(input("Enter number 1 :"))
+b = int(input("Enter number 2 :"))
+
+if b == 0:
+    raise ValueError("please dont divide by 0")
+
+print(f"Please dont divide by zero")
